fa0763ce8bd48627d0a47bbca52583f0deb74621
David Towson <david115632042@gmail.com>
2015-06-20 01:32:32 -0400

    format date data and associate with file name

--- main.py
+++ main.py
@@ -10,8 +10,25 @@ def find_copies(file_list):
             else:
                 print("files %s and %s are different" % (file_list[n], file_list[k]))
 
+def find_later_versions(item_list):
+
+    for ls_line in item_list[1:len(item_list)]:
+        ls_items = ls_line.split(" ")
+        current_date = [ls_items[len(ls_items) - 4],
+                    ls_items[len(ls_items) - 3],
+                    ls_items[len(ls_items) - 2],
+                    ls_items[len(ls_items) - 1]]
+        print(current_date)
+
+
 if __name__ == '__main__':
     output_string = commands.getoutput("ls")
     print(output_string)
     file_list = output_string.splitlines()
     find_copies(file_list)
+
+    output_string = commands.getoutput("ls -l")
+    print(output_string)
+    item_list = output_string.splitlines()
+    find_later_versions(item_list)
+
